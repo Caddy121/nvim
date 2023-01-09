4a93681f0af1dd6eddac56597ca2ce9257ecc4ca
Trinh <trinh.tran@me.com>
2023-01-09 00:39:28 -0800

    change colorscheme

--- lua/user/core/colorscheme.lua
+++ lua/user/core/colorscheme.lua
@@ -1,4 +1,4 @@
-local status, _ = pcall(vim.cmd, "colorscheme duskfox")
+local status, _ = pcall(vim.cmd, "colorscheme horizon")
 if not status then
 	print("Colorscheme not found!") -- print error if colorscheme not installed
 	return
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -376,14 +376,14 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: toggleterm.nvim
-time([[Config for toggleterm.nvim]], true)
-try_loadstring("\27LJ\2\n8\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\15toggleterm\frequire\0", "config", "toggleterm.nvim")
-time([[Config for toggleterm.nvim]], false)
 -- Config for: project.nvim
 time([[Config for project.nvim]], true)
 try_loadstring("\27LJ\2\n>\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\17project_nvim\frequire\0", "config", "project.nvim")
 time([[Config for project.nvim]], false)
+-- Config for: toggleterm.nvim
+time([[Config for toggleterm.nvim]], true)
+try_loadstring("\27LJ\2\n8\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\15toggleterm\frequire\0", "config", "toggleterm.nvim")
+time([[Config for toggleterm.nvim]], false)
 -- Load plugins in order defined by `after`
 time([[Sequenced loading]], true)
 vim.cmd [[ packadd nvim-treesitter ]]
